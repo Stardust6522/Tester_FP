7005e4ff8e6acd4d079e3031b1f0dd1b2b6e4582
Marcos Rivera <112526181+Stardust6522@users.noreply.github.com>
2022-11-20 19:13:30 +0000

    1 to 100

--- main.c
+++ main.c
@@ -2,17 +2,10 @@
 #include <stdlib.h>
 //
 int main(){
-  int i,j;
-  char M[6];
-  M[0]='M';
-  M[1]='a';
-  M[2]='r';
-  M[3]='c';
-  M[4]='o';
-  M[5]='s';
-  printf("\n<<<<<  MI NOMBRE ES  >>>>>\n\n\t\t  ");
-  for(i=0;i<6;i++){
-    printf("%c",M[i]);
+  int i, M[100];
+  for(i=1;i<100;i++){
+    M[i]=i+1;
+    printf("\n %d",M[i]);
   }
   return 0;
 }
