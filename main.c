da4ad88d043e01b457d1a2b9b7605e3745b74b33
Marcos Rivera <112526181+Stardust6522@users.noreply.github.com>
2022-11-20 03:23:29 +0000

    Principal Main int

--- main.c
+++ main.c
@@ -2,23 +2,5 @@
 #include <stdlib.h>
 //
 int main(){
-  int i,j;
-  float M[2][2], s=0;
-  printf("<<<<<  MATRIZ 2x2  >>>>>\n\n");
-  for(i=0;i<2;i++){
-    for(j=0;j<=1;j++){
-    printf("(• Teclear el elemento |%d %d| 5 •)",i+1,j+1);
-    scanf("%f",&M[i][j]);
-      s+=M[i][j];
-    }
-  }
-  printf("\n \t La MATRIZ es:\n\n");
-  for(i=0;i<2;i++){
-    for(j=0;j<=1;j++){
-    printf("\t%.1f",M[i][j]);
-    }
-  printf("\t\n");
-  }
-  printf("\t\n La suma de los elementos  es: %.2f",s);
   return 0;
 }
