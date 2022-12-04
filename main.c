4271bdd93ba4ff8e0b0ab09050fbfe0159e5188d
Marcos Rivera <112526181+Stardust6522@users.noreply.github.com>
2022-12-04 22:21:15 +0000

    Short dates to semi-long dates

--- main.c
+++ main.c
@@ -1,14 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 //
-int main ( )
-{
-int i=10, *i_ptr;
-// Inicialización del apuntador
-printf("\n Acceso directo, var= %d", i);
-printf("\n Acceso indirecto, var= %d", *i_ptr); /*Se despliega la dirección de la
-variable de dos maneras*/
-printf("\n La dirección de var= %d", &i);
-printf("\n LA dirección de var= %d", *i_ptr);
-return 0;
+#include <stdio.h>
+//
+char meses[12][3] = {"Ene","Feb","Mar","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"};
+int i=0, j=0, dia, mes, año;
+//
+int main() {
+  printf("\ningrese una fecha corta: ");
+  scanf("%d %d %d",&dia,&mes,&año);
+  if(año<=0){
+    printf("\naño no válido");
+    return 0;
+  }
+  else if (mes<=0 || mes>12){
+    printf("\nmes no válido");
+    return 0;
+  }
+  if (mes==2){
+    if (dia<0 || dia>28){
+      printf("\ndía no válido");
+      return 0;
+      }
+    }
+  else if (mes==11 || mes==9 || mes==7 || mes==6 || mes==4){
+    if (dia<0 || dia>30){
+      printf("\ndía no válido");
+      return 0;
+      }
+    }
+  else{
+    if(dia<0||dia>31){
+      printf("\ndía no válido");
+      return 0;
+    }
+  }
+  printf("\n\tLa fecha es:\n%d de %c%c%c de %d",dia,meses[mes-1][0],meses[mes-1][1],meses[mes-1][2],año);
+  return 0;
 }
